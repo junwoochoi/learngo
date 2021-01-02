d208935db598efd0949b912729576a950b88044f
junwoochoi <cjw4690@gmail.com>
2021-01-02 15:16:16 +0900

    learn about loop and for "
    ;

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm Done!")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, upperName := lenAndUpper("junwoo")
-
-	fmt.Println(totalLength)
-	fmt.Println(upperName)
+	fmt.Println(superAdd(1, 2, 3, 4, 5, 6))
 }
